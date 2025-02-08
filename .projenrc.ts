66a6c770fa81f259fabb5a57d43802fdda873ff6
Manuel Vogel <8409778+mavogel@users.noreply.github.com>
2025-02-08 14:56:30 +0100

    feat: update ci to dependabot
    Signed-off-by: Manuel Vogel <8409778+mavogel@users.noreply.github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,5 @@
-import { awscdk } from 'projen';
+import { awscdk, ReleasableCommits } from 'projen';
+import { DependabotScheduleInterval } from 'projen/lib/github';
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
@@ -10,13 +11,68 @@ const project = new awscdk.AwsCdkTypeScriptApp({
     'aws-cdk-github-oidc@v2.4.1',
   ],
   autoApproveOptions: {
-    allowedUsernames: ['mavogel'],
+    allowedUsernames: [
+      'dependabot',
+      'dependabot[bot]',
+      'github-bot',
+      'github-actions[bot]',
+      'mvc-bot',
+    ],
+    // The name of the secret that has the GitHub PAT for auto-approving PRs with permissions repo, workflow, write:packages
+    // Generate a new PAT (https://github.com/settings/tokens/new) and add it to your repo's secrets
+    // NOTE: comes from MV-Consulting Org
+    secret: 'PROJEN_GITHUB_TOKEN',
   },
-  autoApproveUpgrades: true,
-  depsUpgradeOptions: {
-    workflowOptions: {
-      labels: ['auto-approve'],
-    },
-  },
+  dependabot: true,
+      dependabotOptions: {
+        scheduleInterval: DependabotScheduleInterval.WEEKLY,
+        labels: ['dependencies', 'auto-approve'],
+        groups: {
+          default: {
+            patterns: ['*'],
+            excludePatterns: ['aws-cdk*', 'projen'],
+          },
+        },
+        ignore: [{ dependencyName: 'aws-cdk-lib' }, { dependencyName: 'aws-cdk' }],
+      },
+      // // See https://github.com/projen/projen/discussions/4040#discussioncomment-11905628
+      releasableCommits: ReleasableCommits.ofType([
+        'feat',
+        'fix',
+        'chore',
+        'refactor',
+        'perf',
+      ]),
+      githubOptions: {
+        pullRequestLintOptions: {
+          semanticTitleOptions: {
+            // see commit types here: https://www.conventionalcommits.org/en/v1.0.0/#summary
+            types: [
+              'feat',
+              'fix',
+              'chore',
+              'refactor',
+              'perf',
+              'docs',
+              'style',
+              'test',
+              'build',
+              'ci',
+            ],
+          },
+        },
+      },
+      versionrcOptions: {
+        types: [
+          { type: 'feat', section: 'Features' },
+          { type: 'fix', section: 'Bug Fixes' },
+          { type: 'chore', section: 'Chores' },
+          { type: 'docs', section: 'Docs' },
+          { type: 'style', hidden: true },
+          { type: 'refactor', hidden: true },
+          { type: 'perf', section: 'Performance' },
+          { type: 'test', hidden: true },
+        ],
+      },
 });
 project.synth();
